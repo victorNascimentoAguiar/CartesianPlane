cb30a25f3415fd6fb5369d86c6c57678a97d30e5
Matheus Braynner <m.braynner@outlook.com>
2023-10-18 15:34:37 -0300

    Exercise completed

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -21,7 +21,7 @@ public class MyPanel extends JPanel {
         String coordinateX = "", coordinateY = "";
 
         int limite = (width / (width / 100));
-        int space = width/30;
+        int scale = 100;
         int valueCoordinate = 0;
 
         g2D.setPaint(Color.black);
@@ -29,37 +29,41 @@ public class MyPanel extends JPanel {
         g2D.drawLine(0, (height / 2), width, (height / 2));
         g2D.drawLine((width / 2), 0, (width / 2), height);
 
-
-        g2D.setPaint(Color.blue);
+        g2D.setPaint(Color.black);
         g2D.fillOval(((width / 2) - (pointSize / 2)), ((height / 2) - (pointSize / 2)), pointSize, pointSize);
 
         // right side
         for (int i = 0; i < limite; i++) {
-            g2D.fillOval((width / 2) - (pointSize / 2) + space, (height / 2) - (pointSize / 2), pointSize, pointSize);
-            g2D.fillOval((width / 2) - (pointSize / 2), (height / 2) - (pointSize / 2) + space, pointSize, pointSize);
+            g2D.setPaint(Color.black);
+            g2D.fillOval((width / 2) - (pointSize / 2) + scale, (height / 2) - (pointSize / 2), pointSize, pointSize);
+            g2D.fillOval((width / 2) - (pointSize / 2), (height / 2) - (pointSize / 2) + scale, pointSize, pointSize);
             coordinateX = coordinateY = String.valueOf(valueCoordinate + 1);
             // Letters
-            g2D.drawString(coordinateX, (width / 2) - (pointSize / 2) + space, (height / 2) + (pointSize * 4));
-            g2D.drawString("-" + coordinateY, (width / 2) - (pointSize * 4), (height / 2) + (pointSize + space));
+            g2D.setPaint(Color.blue);
+            g2D.drawString(coordinateX, (width / 2) - (pointSize / 2) + scale, (height / 2) + (pointSize * 4));
+            g2D.drawString("-" + coordinateY, (width / 2) - (pointSize * 4), (height / 2) + (pointSize + scale));
             // Increment Variables
             valueCoordinate++;
-            space += width / 30;
+            scale += 100;
         }
 
-        space = width / 30;
+        scale = 100;
         valueCoordinate = 0;
         for (int i = limite; i > 0; i--) {
-            // Ovals
-            g2D.fillOval((width / 2) - (pointSize / 2) - space, (height / 2) - (pointSize / 2), pointSize, pointSize);
-            g2D.fillOval((width / 2) - (pointSize / 2), (height / 2) - (pointSize / 2) - space, pointSize, pointSize);
+            g2D.setPaint(Color.black);
+            g2D.fillOval((width / 2) - (pointSize / 2) - scale, (height / 2) - (pointSize / 2), pointSize, pointSize);
+            g2D.fillOval((width / 2) - (pointSize / 2), (height / 2) - (pointSize / 2) - scale, pointSize, pointSize);
             coordinateX = coordinateY = String.valueOf(valueCoordinate + 1);
             // Letters
-            if (i != limite)
-                g2D.drawString("-" + coordinateX, (width / 2) - (pointSize / 2) - space, (height / 2) + (pointSize * 4));
-                g2D.drawString(coordinateY, (width / 2) - (pointSize * 4), (height / 2) + (pointSize - space));
+            g2D.setPaint(Color.blue);
+            g2D.drawString("-" + coordinateX, (width / 2) - (pointSize / 2) - scale, (height / 2) + (pointSize * 4));
+            g2D.drawString(coordinateY, (width / 2) - (pointSize * 4), (height / 2) + (pointSize - scale));
             // Increment Variables
             valueCoordinate++;
-            space += width / 30;
+            scale += 100;
         }
+        scale = 100;
+        g2D.setPaint(Color.red);
+        g2D.drawLine((width / 2) + scale, (height / 2) - scale, (width / 2) + 2 * scale, (height / 2) - 2 * scale);
     }
 }
